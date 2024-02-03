94046c65bed73f5c0071c5faa9cffbad39c22ec6
Janx <janxland@gmail.com>
2024-02-03 22:07:33 +0800

    fix

--- server.js
+++ server.js
@@ -109,7 +109,7 @@ pool.getConnection((err, connection) => {
       });
     });
     // 启动服务器
-    const port = 3000;
+    const port = process.env.PORT || 3000;  
     app.listen(port, () => {
       console.log(`服务器已启动，监听端口 ${port}`);
     });
